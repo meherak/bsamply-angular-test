c1de1d146a2080ef60f9e46cdaaf14bf0128f6d2
meherak <meher.akermi04@gmail.com>
2022-12-10 21:25:21 +0100

    First commit, Project architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CoreModule } from '@core/core.module';
+
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MoviesComponent],
+  imports: [BrowserModule, CoreModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
